0b6edd5c8d3085807434c255aaa25fb9eef4f219
RehanGith <chrehan1007@gmail.com>
2025-01-05 19:59:13 +0500

    Created onCreatedViewHolder and item count function

--- app/src/main/java/com/example/newsapp/adapter/NewsAdapter.kt
+++ app/src/main/java/com/example/newsapp/adapter/NewsAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.newsapp.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
@@ -8,18 +9,22 @@ import com.example.newsapp.databinding.ItemNewsBinding
 import com.example.newsapp.model.Article
 import retrofit2.Response
 
-class NewsAdapter(private val context: Context, private val articleList: Response<List<Article>>):
+class NewsAdapter(private val context: Context, private val articleList: ArrayList<Article>):
     RecyclerView.Adapter<NewsAdapter.ArticleHolder>() {
     class ArticleHolder(binding: ItemNewsBinding) : RecyclerView.ViewHolder(binding.root) {
-
+        val articleImage = binding.articleImage
+        val articleSource = binding.articleSource
+        val articleDescription = binding.articleDescription
+        val articleDateTime = binding.articleDateTime
+        val articleTitle = binding.articleTitle
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleHolder {
-        TODO("Not yet implemented")
+        return ArticleHolder(ItemNewsBinding.inflate(LayoutInflater.from(context), parent, false))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return articleList.size
     }
 
     override fun onBindViewHolder(holder: ArticleHolder, position: Int) {
