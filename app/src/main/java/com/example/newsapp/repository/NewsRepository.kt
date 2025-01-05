9a0f47b4ddf3d6c028f0dc99af7c624b2011cc49
RehanGith <chrehan1007@gmail.com>
2025-01-05 19:42:15 +0500

    Created a Repository class which holds all the function

--- app/src/main/java/com/example/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/repository/NewsRepository.kt
@@ -1,4 +1,21 @@
 package com.example.newsapp.repository
 
-class NewsRepository() {
+import com.example.newsapp.api.RetrofitsInstance
+import com.example.newsapp.database.NewsDatabase
+import com.example.newsapp.model.Article
+
+class NewsRepository(val db: NewsDatabase) {
+
+    suspend fun getHeadlines(countryCode: String, pageNo :Int) =
+        RetrofitsInstance.api.getHeadlines(countryCode, pageNo)
+
+    suspend fun getSearchedArticles(searchQuery: String, pageNo: Int) =
+        RetrofitsInstance.api.getSearchedNews(searchQuery, pageNo)
+
+    suspend fun upsert(article : Article) =
+        db.getArticleDao().upsert(article)
+    fun getAllArticle() =
+        db.getArticleDao().getAllArticle()
+    suspend fun deleteArticle(article: Article) =
+        db.getArticleDao().deleteArticle(article)
 }
