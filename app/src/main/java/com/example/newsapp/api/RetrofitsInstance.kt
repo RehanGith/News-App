bf294d72e1a8142313f3b2dfc2670120ac94bf09
RehanGith <chrehan1007@gmail.com>
2025-01-05 07:42:50 +0500

    Added logging interceptor to retrofit instance

--- app/src/main/java/com/example/newsapp/api/RetrofitsInstance.kt
+++ app/src/main/java/com/example/newsapp/api/RetrofitsInstance.kt
@@ -1,17 +1,27 @@
 package com.example.newsapp.api
 
 import com.example.newsapp.util.Constants.Companion.BASE_URL
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class RetrofitsInstance {
     companion object {
         private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = okhttp3.OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
             Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .baseUrl(BASE_URL)
                 .build()
-                .create(ApiInterface::class.java)
+        }
+        val api by lazy {
+            retrofit.create(ApiInterface::class.java)
         }
     }
 }
