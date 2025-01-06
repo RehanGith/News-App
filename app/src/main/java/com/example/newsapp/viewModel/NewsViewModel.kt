bbfabe7aa2d25c59ff2a63ed40775987b739c3c9
RehanGith <chrehan1007@gmail.com>
2025-01-06 20:00:53 +0500

    Created necessary variable in ViewModel

--- app/src/main/java/com/example/newsapp/viewModel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/viewModel/NewsViewModel.kt
@@ -9,4 +9,10 @@ import com.example.newsapp.util.Responce
 
 class NewsViewModel(app: Application, val newsRepository: NewsRepository): AndroidViewModel(app) {
     val headline: MutableLiveData<Responce<newsResponse>> = MutableLiveData()
+    val headlinesPage  = 1
+    val headlinesResponse: newsResponse? = null
+
+    val searchNews: MutableLiveData<Responce<newsResponse>> = MutableLiveData()
+    val searchNewsPage = 1
+    val searchResponse : newsResponse? = null
 }
