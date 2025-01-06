c886469e9c47b3f8728ed10c592f43ceeb7b80a4
RehanGith <chrehan1007@gmail.com>
2025-01-06 19:50:16 +0500

    Added parameters in viewModel

--- app/src/main/java/com/example/newsapp/viewModel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/viewModel/NewsViewModel.kt
@@ -1,4 +1,8 @@
 package com.example.newsapp.viewModel
 
-class NewsViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.newsapp.repository.NewsRepository
+
+class NewsViewModel(app: Application, val newsRepository: NewsRepository): AndroidViewModel(app) {
 }
