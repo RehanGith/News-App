a2616d8f33d7bca5829ce39ec71d78d8c1ddf467
RehanGith <chrehan1007@gmail.com>
2025-01-07 20:16:59 +0500

    Created an instance of viewModel in MainActivity

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -5,17 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.example.newsapp.database.NewsDatabase
 import com.example.newsapp.databinding.ActivityMainBinding
+import com.example.newsapp.repository.NewsRepository
+import com.example.newsapp.viewModel.NewsViewModel
+import com.example.newsapp.viewModel.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val newsRepository = NewsRepository(NewsDatabase(this))
+        val viewModelFactory = ViewModelFactory(application, newsRepository)
+        viewModel = ViewModelProvider(this, viewModelFactory)[NewsViewModel::class.java]
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentView) as NavHostFragment
         val navController = navHostFragment.navController
         NavigationUI.setupWithNavController(binding.bottomNavView, navController)
