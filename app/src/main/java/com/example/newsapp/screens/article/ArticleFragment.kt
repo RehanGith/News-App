63573337afafe3e5453eba6a1a3baa99071db179
RehanGith <chrehan1007@gmail.com>
2025-01-07 20:26:44 +0500

    Todo work in article fragment

--- app/src/main/java/com/example/newsapp/screens/article/ArticleFragment.kt
+++ app/src/main/java/com/example/newsapp/screens/article/ArticleFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import com.example.newsapp.MainActivity
 import com.example.newsapp.R
 import com.example.newsapp.databinding.FragmentArticleBinding
+import com.example.newsapp.model.Article
 import com.example.newsapp.viewModel.NewsViewModel
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
@@ -16,5 +19,15 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentArticleBinding.bind(view)
+
+        // TODO receive the article through arguments and initialize viewModel
+
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+
+        }
+        binding.fabButton.setOnClickListener{
+
+        }
     }
 }
