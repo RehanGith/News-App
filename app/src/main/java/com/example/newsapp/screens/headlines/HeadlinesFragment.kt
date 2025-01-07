720008c61c1f5a52a12a3b08a0d4556a08a299b1
RehanGith <chrehan1007@gmail.com>
2025-01-07 20:33:19 +0500

    Added important variable for headlineFragments

--- app/src/main/java/com/example/newsapp/screens/headlines/HeadlinesFragment.kt
+++ app/src/main/java/com/example/newsapp/screens/headlines/HeadlinesFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.R
+import com.example.newsapp.adapter.NewsAdapter
+import com.example.newsapp.viewModel.NewsViewModel
 
 
 class HeadlinesFragment : Fragment() {
+    lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var recycleView: RecyclerView
+    lateinit var retryButton: Button
+    lateinit var error: TextView
+    lateinit var itemExecuteError: CardView
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headlines, container, false)
-    }
+
 }
