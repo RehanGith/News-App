f2e69bbf1795b032f995c351ef16ff8d5ea0c85f
RehanGith <chrehan1007@gmail.com>
2025-01-07 21:15:48 +0500

    Added scrollState change fun

--- app/src/main/java/com/example/newsapp/screens/headlines/HeadlinesFragment.kt
+++ app/src/main/java/com/example/newsapp/screens/headlines/HeadlinesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Button
 import android.widget.TextView
 import androidx.cardview.widget.CardView
@@ -78,6 +79,9 @@ class HeadlinesFragment : Fragment() {
 
         override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
             super.onScrollStateChanged(recyclerView, newState)
+            if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
         }
     }
 
